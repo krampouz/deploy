712ca5e8969b4d3579272f928eb4712c7b50efda
krampouz <krampouz@gmail.com>
2020-07-20 00:59:41 +0200

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,10 +1,4 @@
-<#************************************************************************************************************************************
-
-SCRIPT POWERSHELL DEPLOY.PS1
-
-**************************************************************************************************************************************#>
-
-<# Pour executer le script = <strong>Set-ExecutionPolicy RemoteSigned #>
+<# Pour executer le script = Set-ExecutionPolicy RemoteSigned #>
 
 Set-PSDebug -Trace 1
 
@@ -41,17 +35,70 @@ reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v SCRNSAVE.EXE /t REG_SZ /d C
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_SZ /d 0 /F
 
 <# Google GCPW #>
-Invoke-WebRequest -Uri "https://dl.google.com/credentialprovider/gcpwstandaloneenterprise64.msi" -OutFile "c:\informatique\GCPW.msi"
-Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
-Start-Process -FilePath "c:\informatique\GCPW.msi"
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Google\GCPW" /v domains_allowed_to_login /t REG_SZ /d apajh22-29-35.org /F
+
+$domainsAllowedToLogin = "apajh22-29-35.org"
+
+Add-Type -AssemblyName System.Drawing
+Add-Type -AssemblyName PresentationFramework
+
+
+function Is-Admin() {
+$admin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match 'S-1-5-32-544')
+return $admin
+}
+
+<# Check if the current user is an admin and exit if they aren't. #>
+if (-not (Is-Admin)) {
+$result = [System.Windows.MessageBox]::Show('Il faut utiliser un compte ADMIN!', 'GCPW', 'OK', 'Erreur')
+exit 5
+}
+
+<# Choose the GCPW file to download. 32-bit and 64-bit versions have different names #>
+$gcpwFileName = 'gcpwstandaloneenterprise.msi'
+if ([Environment]::Is64BitOperatingSystem) {
+$gcpwFileName = 'gcpwstandaloneenterprise64.msi'
+}
+
+<# Download the GCPW installer. #>
+$gcpwUrlPrefix = 'https://dl.google.com/credentialprovider/'
+$gcpwUri = $gcpwUrlPrefix + $gcpwFileName
+Write-Host 'Telechagement GCPW depuis' $gcpwUri
+Invoke-WebRequest -Uri $gcpwUri -OutFile $gcpwFileName
+
+<# Run the GCPW installer and wait for the installation to finish #>
+$arguments = "/i `"$gcpwFileName`""
+$installProcess = (Start-Process msiexec.exe -ArgumentList $arguments -PassThru -Wait)
+
+<# Check if installation was successful #>
+if ($installProcess.ExitCode -ne 0) {
+$result = [System.Windows.MessageBox]::Show('Installation plantee!', 'GCPW', 'OK', 'Erreur')
+exit $installProcess.ExitCode
+}
+else {
+$result = [System.Windows.MessageBox]::Show('Installation terminee avec succes!', 'GCPW', 'OK', 'Info')
+}
+
+<# Set the required registry key with the allowed domains #>
+$registryPath = 'HKEY_LOCAL_MACHINE\Software\Google\GCPW'
+$name = 'domains_allowed_to_login'
+[microsoft.win32.registry]::SetValue($registryPath, $name, $domainsAllowedToLogin)
+
+$domains = Get-ItemPropertyValue HKLM:\Software\Google\GCPW -Name $name
+
+if ($domains -eq $domainsAllowedToLogin) {
+$msgResult = [System.Windows.MessageBox]::Show('Configuration OK!', 'GCPW', 'OK', 'Info')
+}
+else {
+$msgResult = [System.Windows.MessageBox]::Show('La base de registre est en lecture seule. Configuration KO.', 'GCPW', 'OK', 'Erreur')
+
+}
+
+
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Google\GCPW" /v enable_multi_user_login /t REG_DWORD /d 1 /F
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Google\GCPW" /v enable_dm_enrollment /t REG_DWORD /d 0 /F
 
-<# Drive File Stream #>
-Invoke-WebRequest -Uri "https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.5.2/fusioninventory-agent_windows-x64_2.5.2.exe" -OutFile "c:\informatique\fi252.exe"
-Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
-Start-Process -FilePath "c:\informatique\fi252.exe" -ArgumentList "/acceptlicense /runnow /server='http://intranet.apajh22-29-35.org/glpi/plugins/fusioninventory/' /S /no-ssl-check /add-firewall-exception /execmode=service /installtype=from-scratch"
+
+
 
 <# openshell 4_4_142 #>
 Invoke-WebRequest -Uri "https://github.com/Open-Shell/Open-Shell-Menu/releases/download/v4.4.142/OpenShellSetup_4_4_142.exe" -OutFile "c:\informatique\OpenShellSetup_4_4_142.exe"
@@ -77,3 +124,88 @@ Invoke-WebRequest -Uri "https://www.dwservice.net/download/dwagent_x86.exe" -Out
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 Start-Process -FilePath "c:\informatique\dwagent_x86.exe"
 
+<# Groupe de travail APAJH 22-29-35 #>
+
+Add-Computer -WorkGroupName "APAJH 22-29-35"
+
+<# INFORMATION ORDI + génération rapport html#>
+
+
+
+Get-CimInstance -ClassName Win32_ComputerSystem
+
+
+#CSS codes
+$header = @"
+<style>
+    h1 {
+        font-family: Arial, Helvetica, sans-serif;
+        color: #e68a00;
+        font-size: 28px;
+    }
+    h2 {
+        font-family: Arial, Helvetica, sans-serif;
+        color: #000099;
+        font-size: 16px;
+    }
+   table {
+		font-size: 12px;
+		border: 0px; 
+		font-family: Arial, Helvetica, sans-serif;
+	} 
+    td {
+		padding: 4px;
+		margin: 0px;
+		border: 0;
+	}
+    th {
+        background: #395870;
+        background: linear-gradient(#49708f, #293f50);
+        color: #fff;
+        font-size: 11px;
+        text-transform: uppercase;
+        padding: 10px 15px;
+        vertical-align: middle;
+	}
+    tbody tr:nth-child(even) {
+        background: #f0f0f2;
+    }
+    #CreationDate {
+        font-family: Arial, Helvetica, sans-serif;
+        color: #ff3300;
+        font-size: 12px;
+    }
+    .StopStatus {
+        color: #ff0000;
+    }
+    .RunningStatus {
+        color: #008000;
+    }
+</style>
+"@
+#The command below will get the name of the computer
+$ComputerName = "<h1>NOM ORDI: $env:computername</h1>"
+#The command below will get the Operating System information, convert the result to HTML code as table and store it to a variable
+$OSinfo = Get-CimInstance -Class Win32_OperatingSystem | ConvertTo-Html -As List -Property Version,Caption,BuildNumber,Manufacturer -Fragment -PreContent "<h2>OS Information</h2>"
+
+#The command below will get the Processor information, convert the result to HTML code as table and store it to a variable
+$ProcessInfo = Get-CimInstance -ClassName Win32_Processor | ConvertTo-Html -As List -Property DeviceID,Name,Caption,MaxClockSpeed,SocketDesignation,Manufacturer -Fragment -PreContent "<h2>Processeur Information</h2>"
+
+#The command below will get the BIOS information, convert the result to HTML code as table and store it to a variable
+$BiosInfo = Get-CimInstance -ClassName Win32_BIOS | ConvertTo-Html -As List -Property SMBIOSBIOSVersion,Manufacturer,Name,SerialNumber -Fragment -PreContent "<h2>BIOS Information</h2>"
+
+#The command below will get the details of Disk, convert the result to HTML code as table and store it to a variable
+$DiscInfo = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" | ConvertTo-Html -As List -Property DeviceID,DriveType,ProviderName,VolumeName,Size,FreeSpace -Fragment -PreContent "<h2>Disk Information</h2>"
+
+#The command below will get first 10 services information, convert the result to HTML code as table and store it to a variable
+$ServicesInfo = Get-CimInstance -ClassName Win32_Service | Select-Object -First 10  |ConvertTo-Html -Property Name,DisplayName,State -Fragment -PreContent "<h2>Services Information</h2>"
+$ServicesInfo = $ServicesInfo -replace '<td>Running</td>','<td class="RunningStatus">Running</td>'
+$ServicesInfo = $ServicesInfo -replace '<td>Stopped</td>','<td class="StopStatus">Stopped</td>'
+
+  
+#The command below will combine all the information gathered into a single HTML report
+$Report = ConvertTo-HTML -Body "$ComputerName $OSinfo $ProcessInfo $BiosInfo $DiscInfo $ServicesInfo" -Head $header -Title "Computer Information Report" -PostContent "<p id='CreationDate'>Date édition: $(Get-Date)</p>"
+
+#The command below will generate the report to an HTML file
+$Report | Out-File .\\Basic-Ordi-Rapport.html
+Start-Process "Basic-Ordi-Rapport.html"
